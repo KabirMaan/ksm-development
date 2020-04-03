dac0bc4df3f19c7f8960efedcd0002812b2b9474
Kabir Maan <kabirmaan9@gmail.com>
2020-04-03 15:02:06 +0100

    Setup Grid with animation on LandingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Lottie from "react-lottie";
 import animationData from "../animations/landinganimation/data";
 import { makeStyles } from "@material-ui/styles";
+import { Grid, Button } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({}));
 
@@ -15,7 +16,28 @@ const LandingPage = () => {
       preserveAspectRatio: "xMidYMid slice"
     }
   };
-  return <Lottie options={defaultOptions} height={"100%"} width={"100%"} />;
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Grid container direction="row">
+          <Grid item>
+            <div>Expert Software Development</div>
+            <Grid container>
+              <Grid item>
+                <Button variant="contained">Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined">Learn More</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LandingPage;
